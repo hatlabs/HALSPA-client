eacd364cd7fdeafebf3abce2b1536c1ec69e2cc6
Matti Airas <matti.airas@hatlabs.fi>
2025-06-26 13:58:01 +0300

    Sauce fixes

--- sauce/sauce/adc.py
+++ sauce/sauce/adc.py
@@ -15,7 +15,31 @@ class ADCChannel:
     def read_raw(self):
         return self.adc.read(channel1=self.adc_channel)
 
-    def read_v(self):
+    def read_voltage(self):
+        return self.scale * self.adc.raw_to_v(self.read_raw())
+
+
+class ADCDiff:
+    def __init__(
+        self,
+        adc: ads1x15.ADS1115,
+        adc_channel1: int,
+        adc_channel2: int,
+        scale=1.0,
+        rb=None,
+    ):
+        self.adc = adc
+        self.adc_channel1 = adc_channel1
+        self.adc_channel2 = adc_channel2
+        self.scale = scale
+        if rb is not None:
+            rt = 100e3 - rb
+            self.scale = self.scale * (rt + rb) / rb
+
+    def read_raw(self):
+        return self.adc.read(channel1=self.adc_channel1, channel2=self.adc_channel2)
+
+    def read_voltage(self):
         return self.scale * self.adc.raw_to_v(self.read_raw())
 
 
@@ -28,6 +52,6 @@ class AnalogMuxADCChannel:
         self.select_pin()
         return self.adc_channel.read_raw()
 
-    def read_v(self):
+    def read_voltage(self):
         self.select_pin()
-        return self.adc_channel.read_v()
+        return self.adc_channel.read_voltage()
--- sauce/sauce/sauce.py
+++ sauce/sauce/sauce.py
@@ -1,6 +1,11 @@
 from machine import I2C, Pin
 from micropython import const
 
+from sauce.adc import ads1x15
+from sauce.analog_mux import AnalogMux
+from sauce.power_control import PowerControl
+from sauce.tca9535 import TCA9535
+
 SDA_PIN = const(20)
 SCL_PIN = const(21)
 
@@ -12,10 +17,10 @@ DIGEXP2_ADDR = const(0x23)
 
 i2c = I2C(0, scl=Pin(SCL_PIN), sda=Pin(SDA_PIN))
 
-# powcon = PowerControl(i2c)
-# anamux = AnalogMux(i2c)
-# ads1115_1 = ads1x15.ADS1115(i2c, ADC1_ADDR, 1)
-# ads1115_2 = ads1x15.ADS1115(i2c, ADC2_ADDR, 1)
+powcon = PowerControl(i2c)
+anamux = AnalogMux(i2c)
+ads1115_1 = ads1x15.ADS1115(i2c, ADC1_ADDR, 1)
+ads1115_2 = ads1x15.ADS1115(i2c, ADC2_ADDR, 1)
 
-# digexp1 = TCA9535(i2c, address=DIGEXP1_ADDR)
-# digexp2 = TCA9535(i2c, address=DIGEXP2_ADDR)
+digexp1 = TCA9535(i2c, address=DIGEXP1_ADDR)
+digexp2 = TCA9535(i2c, address=DIGEXP2_ADDR)
